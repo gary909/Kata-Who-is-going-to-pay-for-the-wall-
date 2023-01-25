4f0f4905c13c4607f4e9cb430ddc1aada59c3b3c
Gary White <gary@iMac-2017.local>
2023-01-25 12:44:25 +0100

    code

---
+++ app.js
@@ -0,0 +1,10 @@
+function whoIsPaying(name){
+    //your code here
+}
+
+console.log(whoIsPaying("Mexico")); // ["Mexico", "Me"]
+console.log(whoIsPaying("Melania")); // ["Melania", "Me"]
+console.log(whoIsPaying("Melissa")); // ["Melissa", "Me"]
+console.log(whoIsPaying("Me")); // ["Mexico", "Me"]
+console.log(whoIsPaying("")); // [""]
+console.log(whoIsPaying("I")); // ["I"]
